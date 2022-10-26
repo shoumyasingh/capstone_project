430964a5aa84a3f99a2ad0dbaf54f10dd7529c0a
Shoumya Singh <shoumya25@gmail.com>
2022-10-25 20:29:04 -0700

    Removed email from settings

--- Housepedia/settings.py
+++ Housepedia/settings.py
@@ -147,13 +147,6 @@ MESSAGE_TAGS = {
 }
 
 
-# Email config
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT= 587
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSORD=''
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
