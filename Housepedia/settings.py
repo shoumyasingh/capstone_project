41af4b8db6946c98e6253f42e0bf3c43b287dbab
Shoumya Singh <shoumya25@gmail.com>
2022-11-07 21:33:47 -0800

    modified

--- Housepedia/settings.py
+++ Housepedia/settings.py
@@ -152,7 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER='shoumyasingh1408@gmail.com'
-EMAIL_HOST_PASSORD='Hello!world1408'
+EMAIL_HOST_PASSORD='#####'
 EMAIL_USE_TLS=True
 
 # try:
